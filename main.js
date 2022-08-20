74417c9ea92a298327c97ca938a8ed296c90a89e
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2022-08-20 15:08:23 +1200

    Add D3

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import './style.css'
+import * as d3 from 'd3'
+
+d3.selectAll('td')
+  .transition()
+  .style('background-color', (elem, idx) => idx === 0 ? 'gray' : 'white')
 
 console.log('Hello!')
