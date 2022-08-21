3a40c03f89a48e2fe28fae7fea72a89fc290918c
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2022-08-21 15:59:47 +1200

    Use graph-scroll

--- README.md
+++ README.md
@@ -5,11 +5,13 @@ A simple D3.js scroller.
 Interactive visualization as you scroll in your browser. Supports
 responsive view ports.
 
-SCSS preprocessing enabled.
+SCSS preprocessing enabled. Semantic web tags used too. Tested for
+accessibility.
 
 ## References
 
 - “How I Created an Interactive, Scrolling Visualisation with D3.js, and how you can too”. Cuthbert Chow. 2022-08-20. <https://web.archive.org/web/20220820024830/https://towardsdatascience.com/how-i-created-an-interactive-scrolling-visualisation-with-d3-js-and-how-you-can-too-e116372e2c73?gi=a3b978d220b1>
 - “So You Want to Build A Scroller”. Jim Vallandingham. 2022-08-20. <https://web.archive.org/web/20220628203500/https://vallandingham.me/scroller.html>
 - “D3Js Scroller”. Carlos Aguni. 2022-08-20. <https://web.archive.org/web/20220820024321/https://crashlaker.github.io/2021/09/18/d3js_scroller.html>
-- “D3.js”. D3.js Community. 2022-08-20. https://d3js.org/
+- “graph-scroll”. Adam Pearce. 2022-08-21. <https://github.com/1wheel/graph-scroll>
+- “D3.js”. D3.js Community. 2022-08-20. <https://d3js.org/>
--- main.js
+++ main.js
@@ -1,6 +1,68 @@
 import './style.scss'
-import scroller from "./scroller";
+import * as d3 from 'd3'
+import graphScroll from "./graph-scroll"
 
+let oldWidth = 0
+const render = () => {
+  if (oldWidth === innerWidth) return
+  oldWidth = innerWidth
+  let width
+  let height
+  width = d3.select('#graph').node().offsetWidth
+  height = width
+  const r = 40
 
+  if (innerWidth <= 925) {
+    width = innerWidth
+    height = innerHeight * .7
+  }
+
+  const svg = d3.select('#graph').html('')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+
+  const circle = svg
+    .append('circle')
+    .attr('cx', 0)
+    .attr('cy', 0)
+    .attr('r', r)
+
+  const colors = ['orange', 'purple', 'steelblue', 'pink', 'black']
+  const positions = [
+    {cx: width - r, cy: r},
+    {cx: r, cy: r},
+    {cx: width - r, cy: height - r},
+    {cx: width / 2, cy: height / 2}
+  ]
+  const gs = graphScroll()
+    .container(d3.select('.container-1'))
+    .graph(d3.selectAll('.container-1 #graph'))
+    .eventId('uniqueId1')  // namespace for scroll and resize events
+    .sections(d3.selectAll('.container-1 #sections > div'))
+    // .offset(innerWidth < 900 ? innerHeight - 30 : 200)
+    .on('active', function (i) {
+      const position = positions[i]
+
+      circle
+        .transition()
+        .duration(1000)
+        .attr('cx', position['cx'])
+        .attr('cy', position['cy'])
+        .transition()
+        .style('fill', colors[i])
+    })
+
+  d3
+    .select('#source')
+    .style('margin-bottom', `${window.innerHeight - 450}px`)
+    .style('padding', `100px`)
+}
+
+d3
+  .select(window)
+  .on('resize', render)
+
+render()
 
 console.log('Hello!')
